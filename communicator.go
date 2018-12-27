b3a218b013798917efe13031a2da9438ebec2c72
raghu.chandramoule <raghu.chandramoule@ASPIRE1787>
2018-12-27 15:57:31 +0530

    Formatted files using goFmt -w

--- communicator.go
+++ communicator.go
@@ -1,230 +1,230 @@
 package main
 
 import (
-        "encoding/json"
-        "fmt"
-        "io/ioutil"
-        "net/http"
-        "net/url"
-        "strings"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
 )
 
 // CreateRequest creates http request for the jiraUrl from config and path passed
 func CreateRequest(jiraUrl string, apiPath string, authToken string, params []string) *http.Request {
-        var finalPath string
-        bearer := "Basic " + authToken
-        if params != nil {
-                var endPoint *url.URL
-                endPoint, err := url.Parse(jiraUrl)
-                HandleError(err)
+	var finalPath string
+	bearer := "Basic " + authToken
+	if params != nil {
+		var endPoint *url.URL
+		endPoint, err := url.Parse(jiraUrl)
+		HandleError(err)
 
-                endPoint.Path += apiPath
-                parameters := url.Values{}
+		endPoint.Path += apiPath
+		parameters := url.Values{}
 
-                for _, param := range params {
-                        parameters.Add("jql", param)
-                        parameters.Add("fields", param)
-                }
+		for _, param := range params {
+			parameters.Add("jql", param)
+			parameters.Add("fields", param)
+		}
 
-                endPoint.RawQuery = parameters.Encode()
-                finalPath = endPoint.String()
+		endPoint.RawQuery = parameters.Encode()
+		finalPath = endPoint.String()
 
-        } else {
-                finalPath = jiraUrl + apiPath
-        }
+	} else {
+		finalPath = jiraUrl + apiPath
+	}
 
-        req, err := http.NewRequest("GET", finalPath, nil)
-        req.Header.Add("Authorization", bearer)
-        HandleError(err)
+	req, err := http.NewRequest("GET", finalPath, nil)
+	req.Header.Add("Authorization", bearer)
+	HandleError(err)
 
-        return req
+	return req
 
 }
 
 // CreateRequestAndGetResponse creates http request for the jiraUrl from config and path passed and gets the response body
 func CreateRequestAndGetResponse(jiraUrl string, apiPath string, authToken string, params []string) []byte {
 
-        req := CreateRequest(jiraUrl, apiPath, authToken, params)
-        client := &http.Client{}
-        resp, err := client.Do(req)
-        HandleError(err)
+	req := CreateRequest(jiraUrl, apiPath, authToken, params)
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	HandleError(err)
 
-        defer resp.Body.Close()
-        body, err := ioutil.ReadAll(resp.Body)
-        HandleError(err)
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	HandleError(err)
 
-        return body
+	return body
 }
 
 // GetCustomFields gets all the custom fields for the jiraUrl mentioned in the config
 func GetCustomFields(config Configuration, customFieldChannel chan map[string]string) {
 
-        body := CreateRequestAndGetResponse(config.JiraUrl, "/rest/api/2/field", config.AuthToken, nil)
-        var fields []map[string]interface{}
-        json.Unmarshal([]byte(body), &fields)
+	body := CreateRequestAndGetResponse(config.JiraUrl, "/rest/api/2/field", config.AuthToken, nil)
+	var fields []map[string]interface{}
+	json.Unmarshal([]byte(body), &fields)
 
-        var result map[string]string
-        result = make(map[string]string)
+	var result map[string]string
+	result = make(map[string]string)
 
-        for _, field := range fields {
-                if field["custom"].(bool) {
-                        _, ok := result[field["name"].(string)]
-                        if !ok {
-                                result[strings.ToLower(field["name"].(string))] = strings.ToLower(field["id"].(string))
-                        }
-                } else {
-                        _, ok := result[strings.ToLower(field["name"].(string))]
-                        if ok {
-                                delete(result, strings.ToLower(field["name"].(string)))
-                        }
-                }
-        }
+	for _, field := range fields {
+		if field["custom"].(bool) {
+			_, ok := result[field["name"].(string)]
+			if !ok {
+				result[strings.ToLower(field["name"].(string))] = strings.ToLower(field["id"].(string))
+			}
+		} else {
+			_, ok := result[strings.ToLower(field["name"].(string))]
+			if ok {
+				delete(result, strings.ToLower(field["name"].(string)))
+			}
+		}
+	}
 
-        customFieldChannel <- result
+	customFieldChannel <- result
 }
 
 // SearchIssues finds issues based on the jql passed
 func SearchIssues(config Configuration, jql string, processedFields []string, issueRetrievedChannel chan JiraIssue) {
 
-        params := make([]string, 0)
-        params = append(params, jql)
-        params = append(params, strings.Join(processedFields, ","))
+	params := make([]string, 0)
+	params = append(params, jql)
+	params = append(params, strings.Join(processedFields, ","))
 
-        body := CreateRequestAndGetResponse(config.JiraUrl, "/rest/api/2/search", config.AuthToken, params)
-        var responseResult map[string]interface{}
-        var issues []interface{}
-        json.Unmarshal([]byte(body), &responseResult)
+	body := CreateRequestAndGetResponse(config.JiraUrl, "/rest/api/2/search", config.AuthToken, params)
+	var responseResult map[string]interface{}
+	var issues []interface{}
+	json.Unmarshal([]byte(body), &responseResult)
 
-        issues = responseResult["issues"].([]interface{})
+	issues = responseResult["issues"].([]interface{})
 
-        for _, issue := range issues {
-                jiraIssue := JiraIssue{Data: issue.(map[string]interface{}), Fields: processedFields}
-                issueRetrievedChannel <- jiraIssue
-        }
+	for _, issue := range issues {
+		jiraIssue := JiraIssue{Data: issue.(map[string]interface{}), Fields: processedFields}
+		issueRetrievedChannel <- jiraIssue
+	}
 
 }
 
 // GetIssue fetches Issue based from the jiraUrl in the config and issueId passed
 func GetIssue(config Configuration, issueId string, includeChangeLog bool) map[string]interface{} {
 
-        var getIssueUrl string
+	var getIssueUrl string
 
-        if includeChangeLog {
-                getIssueUrl = "/rest/api/2/issue/" + issueId + "?expand=changelog"
-        } else {
-                getIssueUrl = "/rest/api/2/issue/" + issueId
-        }
+	if includeChangeLog {
+		getIssueUrl = "/rest/api/2/issue/" + issueId + "?expand=changelog"
+	} else {
+		getIssueUrl = "/rest/api/2/issue/" + issueId
+	}
 
-        body := CreateRequestAndGetResponse(config.JiraUrl, getIssueUrl, config.AuthToken, nil)
+	body := CreateRequestAndGetResponse(config.JiraUrl, getIssueUrl, config.AuthToken, nil)
 
-        var responseResult map[string]interface{}
-        json.Unmarshal([]byte(body), &responseResult)
+	var responseResult map[string]interface{}
+	json.Unmarshal([]byte(body), &responseResult)
 
-        return responseResult
+	return responseResult
 }
 
 // GetSubTasksForIssue gets All Sub Tasks for the passed issue
 func GetSubTasksForIssue(config Configuration, issue JiraIssue, finalIssueChannel chan JiraIssue, includeChangeLog bool, totalRestCalls *int) {
 
-        issueId := issue.Data["id"].(string)
-        *totalRestCalls++
-        parent := GetIssue(config, issueId, includeChangeLog)
-        subTasks := parent["fields"].(map[string]interface{})["subtasks"].([]interface{})
-        result := make([]SubTask, 0)
+	issueId := issue.Data["id"].(string)
+	*totalRestCalls++
+	parent := GetIssue(config, issueId, includeChangeLog)
+	subTasks := parent["fields"].(map[string]interface{})["subtasks"].([]interface{})
+	result := make([]SubTask, 0)
 
-        for _, subTask := range subTasks {
-                *totalRestCalls++
-                subTaskIssue := GetIssue(config, subTask.(map[string]interface{})["id"].(string), false)
-                assignee := GetValueFromField(subTaskIssue, "assignee")
-                issueType := GetValueFromField(subTaskIssue, "issuetype")
-                name := GetValueFromField(subTaskIssue, "summary")
-                totalHours := GetValueFromField(subTaskIssue, "timetracking")
-                currentSubTask := SubTask{Type: issueType, Name: name, AssigneeName: assignee, TotalHours: totalHours}
+	for _, subTask := range subTasks {
+		*totalRestCalls++
+		subTaskIssue := GetIssue(config, subTask.(map[string]interface{})["id"].(string), false)
+		assignee := GetValueFromField(subTaskIssue, "assignee")
+		issueType := GetValueFromField(subTaskIssue, "issuetype")
+		name := GetValueFromField(subTaskIssue, "summary")
+		totalHours := GetValueFromField(subTaskIssue, "timetracking")
+		currentSubTask := SubTask{Type: issueType, Name: name, AssigneeName: assignee, TotalHours: totalHours}
 
-                result = append(result, currentSubTask)
-        }
+		result = append(result, currentSubTask)
+	}
 
-        issue.SubTasks = result
+	issue.SubTasks = result
 
-        parentIssueType := GetValueFromField(parent, "issuetype")
-        if parentIssueType == "Bug" || parentIssueType == "Functional Bug" {
-                issue.AssigneeName = GetDeveloperNameFromLog(parent)
-        }
+	parentIssueType := GetValueFromField(parent, "issuetype")
+	if parentIssueType == "Bug" || parentIssueType == "Functional Bug" {
+		issue.AssigneeName = GetDeveloperNameFromLog(parent)
+	}
 
-        finalIssueChannel <- issue
+	finalIssueChannel <- issue
 
 }
 
 // GetDeveloperNameFromLog gets Developer Name From the work log record where status was 'In Development' stage
 func GetDeveloperNameFromLog(issue map[string]interface{}) string {
-        developerName := ""
-        histories := issue["changelog"].(map[string]interface{})["histories"].([]interface{})
-        for _, history := range histories {
-                mapHistory := history.(map[string]interface{})
-                items := mapHistory["items"].([]interface{})
-                for _, item := range items {
-                        strInDevelopment, ok := item.(map[string]interface{})["toString"].(string)
-                        if ok && strInDevelopment == "In Development" {
-                                developerName = mapHistory["author"].(map[string]interface{})["displayName"].(string)
-                                break
-                        }
-                }
+	developerName := ""
+	histories := issue["changelog"].(map[string]interface{})["histories"].([]interface{})
+	for _, history := range histories {
+		mapHistory := history.(map[string]interface{})
+		items := mapHistory["items"].([]interface{})
+		for _, item := range items {
+			strInDevelopment, ok := item.(map[string]interface{})["toString"].(string)
+			if ok && strInDevelopment == "In Development" {
+				developerName = mapHistory["author"].(map[string]interface{})["displayName"].(string)
+				break
+			}
+		}
 
-                if developerName != "" {
-                        break
-                }
-        }
+		if developerName != "" {
+			break
+		}
+	}
 
-        return developerName
+	return developerName
 
 }
 
 // GetValueFromField gets the value from the 'fields' property of the issue
 func GetValueFromField(issue map[string]interface{}, field string) string {
-        val, ok := issue["fields"]
-        if ok {
-                fieldsMap := val.(map[string]interface{})
+	val, ok := issue["fields"]
+	if ok {
+		fieldsMap := val.(map[string]interface{})
 
-                val, ok := fieldsMap[field]
-                if ok {
-                        return strings.Replace(GetValue(val, field), ",", "", -1)
-                }
-        }
-        return "N/A"
+		val, ok := fieldsMap[field]
+		if ok {
+			return strings.Replace(GetValue(val, field), ",", "", -1)
+		}
+	}
+	return "N/A"
 }
 
 // GetValue gets the value based on the type of interface
 func GetValue(val interface{}, fieldName string) string {
-        var result string
-        arrayVal, isArray := val.([]interface{})
-        mapVal, isMap := val.(map[string]interface{})
-        if isArray {
-                result = arrayVal[0].(map[string]interface{})["value"].(string)
-        } else if isMap {
-                tmpResult, ok := mapVal[GetNestedMapKeyName(fieldName)]
-                if ok {
-                        result = tmpResult.(string)
-                }
-        } else if val != nil {
-                result = fmt.Sprint(val)
-        }
+	var result string
+	arrayVal, isArray := val.([]interface{})
+	mapVal, isMap := val.(map[string]interface{})
+	if isArray {
+		result = arrayVal[0].(map[string]interface{})["value"].(string)
+	} else if isMap {
+		tmpResult, ok := mapVal[GetNestedMapKeyName(fieldName)]
+		if ok {
+			result = tmpResult.(string)
+		}
+	} else if val != nil {
+		result = fmt.Sprint(val)
+	}
 
-        return result
+	return result
 }
 
 // GetNestedMapKeyName gets the nested field name to search for a parent name
 func GetNestedMapKeyName(fieldName string) string {
-        if fieldName == "assignee" {
-                return "displayName"
-        }
+	if fieldName == "assignee" {
+		return "displayName"
+	}
 
-        if fieldName == "issuetype" {
-                return "name"
-        }
+	if fieldName == "issuetype" {
+		return "name"
+	}
 
-        if fieldName == "timetracking" {
-                return "originalEstimate"
-        }
+	if fieldName == "timetracking" {
+		return "originalEstimate"
+	}
 
-        return ""
+	return ""
 }
--- jiraSearch.go
+++ jiraSearch.go
@@ -1,213 +1,213 @@
 package main
 
 import (
-        "fmt"
-        "strings"
-        "time"
+	"fmt"
+	"strings"
+	"time"
 
-        ui "github.com/gizak/termui"
+	ui "github.com/gizak/termui"
 )
 
 // ProcessedData represents the filters and fields after replacing custom field ids
 type ProcessedData struct {
-        filters map[string]string
-        fields  []string
+	filters map[string]string
+	fields  []string
 }
 
 // Configuration represents the configuration file 'config.json'
 type Configuration struct {
-        JiraUrl          string
-        Credentials      Credentials
-        Filters          map[string]interface{}
-        FieldsToRetrieve []string
-        DownloadPath     string
-        AuthToken        string
+	JiraUrl          string
+	Credentials      Credentials
+	Filters          map[string]interface{}
+	FieldsToRetrieve []string
+	DownloadPath     string
+	AuthToken        string
 }
 
 // Credentials of the user
 type Credentials struct {
-        Username string
-        Password string
+	Username string
+	Password string
 }
 
 // SubTask of a Jira Issue
 type SubTask struct {
-        Type         string
-        AssigneeName string
-        TotalHours   string
-        Name         string
+	Type         string
+	AssigneeName string
+	TotalHours   string
+	Name         string
 }
 
 // JiraIssue represents an issue in Jira
 type JiraIssue struct {
-        Data         map[string]interface{}
-        SubTasks     []SubTask
-        Fields       []string
-        AssigneeName string
+	Data         map[string]interface{}
+	SubTasks     []SubTask
+	Fields       []string
+	AssigneeName string
 }
 
 func main() {
-        start := time.Now()
+	start := time.Now()
 
-        // Initialize necessary variables
-        customFieldChannel := make(chan map[string]string)
-        customFieldProcessorChannel := make(chan ProcessedData)
-        issueRetrievedChannel := make(chan JiraIssue)
-        finalIssueChannel := make(chan JiraIssue)
-        outputData := make([][]string, 0)
-        issueCount := 0
-        totalRestCalls := 0
-        totalIssueCount := 0
+	// Initialize necessary variables
+	customFieldChannel := make(chan map[string]string)
+	customFieldProcessorChannel := make(chan ProcessedData)
+	issueRetrievedChannel := make(chan JiraIssue)
+	finalIssueChannel := make(chan JiraIssue)
+	outputData := make([][]string, 0)
+	issueCount := 0
+	totalRestCalls := 0
+	totalIssueCount := 0
 
-        // Init the termui
-        // for displaying the output statistics in terminal
-        err := ui.Init()
-        HandleError(err)
-        defer ui.Close()
-        g, bc := GetProgressAndStatisticsBar()
+	// Init the termui
+	// for displaying the output statistics in terminal
+	err := ui.Init()
+	HandleError(err)
+	defer ui.Close()
+	g, bc := GetProgressAndStatisticsBar()
 
-        // Read config from the 'config.json' file
-        config := ReadConfigFromFile("config.json")
+	// Read config from the 'config.json' file
+	config := ReadConfigFromFile("config.json")
 
-        // get all the custom fields first and populate in customFieldChannel
-        totalRestCalls++
-        go GetCustomFields(config, customFieldChannel)
+	// get all the custom fields first and populate in customFieldChannel
+	totalRestCalls++
+	go GetCustomFields(config, customFieldChannel)
 
-        // listen to custom fields channel and process the data
-        go ProcessCustomFields(config, customFieldChannel, customFieldProcessorChannel)
+	// listen to custom fields channel and process the data
+	go ProcessCustomFields(config, customFieldChannel, customFieldProcessorChannel)
 
-        // listen to processed data and intiate the search based on it
-        go InitiateSearch(config, customFieldProcessorChannel, issueRetrievedChannel, &totalRestCalls)
+	// listen to processed data and intiate the search based on it
+	go InitiateSearch(config, customFieldProcessorChannel, issueRetrievedChannel, &totalRestCalls)
 
-        //prepare output with the headers first
-        headers := config.FieldsToRetrieve
-        outputData = append(outputData, headers)
+	//prepare output with the headers first
+	headers := config.FieldsToRetrieve
+	outputData = append(outputData, headers)
 
-        for {
-                select {
+	for {
+		select {
 
-                case issue := <-issueRetrievedChannel:
-                        {
-                                totalIssueCount++
-                                issueCount++
-                                DisplayProgressAndStatistics(totalIssueCount, -1, totalRestCalls, int(time.Since(start).Seconds()), g, bc)
-                                includeChangeLog := strings.Contains(config.Filters["IssueType"].(string), "Bug")
+		case issue := <-issueRetrievedChannel:
+			{
+				totalIssueCount++
+				issueCount++
+				DisplayProgressAndStatistics(totalIssueCount, -1, totalRestCalls, int(time.Since(start).Seconds()), g, bc)
+				includeChangeLog := strings.Contains(config.Filters["IssueType"].(string), "Bug")
 
-                                // listen to issue retrieved channel and assign the sub tasks for it
-                                go GetSubTasksForIssue(config, issue, finalIssueChannel, includeChangeLog, &totalRestCalls)
-                        }
+				// listen to issue retrieved channel and assign the sub tasks for it
+				go GetSubTasksForIssue(config, issue, finalIssueChannel, includeChangeLog, &totalRestCalls)
+			}
 
-                case finalIssue := <-finalIssueChannel:
-                        {
-                                issueCount--
-                                fieldValues := make([]string, 0)
+		case finalIssue := <-finalIssueChannel:
+			{
+				issueCount--
+				fieldValues := make([]string, 0)
 
-                                // Listen to final populated issue and prepare the output for all the fields mentioned in the configuration
-                                for _, field := range finalIssue.Fields {
-                                        val, ok := finalIssue.Data[field]
-                                        if ok {
-                                                fieldValues = append(fieldValues, strings.Replace(val.(string), ",", "", -1))
-                                        } else {
-                                                fieldValues = append(fieldValues, GetFieldValue(field, finalIssue))
-                                        }
-                                }
-                                if len(fieldValues) > 0 {
-                                        outputData = append(outputData, fieldValues)
-                                }
+				// Listen to final populated issue and prepare the output for all the fields mentioned in the configuration
+				for _, field := range finalIssue.Fields {
+					val, ok := finalIssue.Data[field]
+					if ok {
+						fieldValues = append(fieldValues, strings.Replace(val.(string), ",", "", -1))
+					} else {
+						fieldValues = append(fieldValues, GetFieldValue(field, finalIssue))
+					}
+				}
+				if len(fieldValues) > 0 {
+					outputData = append(outputData, fieldValues)
+				}
 
-                                DisplayProgressAndStatistics(totalIssueCount, issueCount, totalRestCalls, int(time.Since(start).Seconds()), g, bc)
+				DisplayProgressAndStatistics(totalIssueCount, issueCount, totalRestCalls, int(time.Since(start).Seconds()), g, bc)
 
-                        }
+			}
 
-                        if issueCount == 0 {
-                                // Once all the issues are processed, flush out the output data to the csv file mentioned in the configuration 'DownloadPath'
-                                WriteToCsv(outputData, config.DownloadPath)
-                                DisplayProgressAndStatistics(totalIssueCount, issueCount, totalRestCalls, int(time.Since(start).Seconds()), g, bc)
-                                fmt.Println("Download complete!!")
-                                break
-                        }
+			if issueCount == 0 {
+				// Once all the issues are processed, flush out the output data to the csv file mentioned in the configuration 'DownloadPath'
+				WriteToCsv(outputData, config.DownloadPath)
+				DisplayProgressAndStatistics(totalIssueCount, issueCount, totalRestCalls, int(time.Since(start).Seconds()), g, bc)
+				fmt.Println("Download complete!!")
+				break
+			}
 
-                }
-        }
+		}
+	}
 }
 
 // GetJql constructs the JQL string based on filters in the configuration
 func GetJql(filters map[string]string) string {
-        index := 0
-        totalCount := len(filters)
-        var b strings.Builder
-        for k, v := range filters {
-                index++
-                if strings.Contains(v, ",") {
-                        valSlice := strings.Split(v, ",")
-                        b.WriteString(k + " in (" + GetInFilterValue(valSlice) + ")")
-                } else {
-                        b.WriteString(k + "=" + "'" + v + "'")
-                }
+	index := 0
+	totalCount := len(filters)
+	var b strings.Builder
+	for k, v := range filters {
+		index++
+		if strings.Contains(v, ",") {
+			valSlice := strings.Split(v, ",")
+			b.WriteString(k + " in (" + GetInFilterValue(valSlice) + ")")
+		} else {
+			b.WriteString(k + "=" + "'" + v + "'")
+		}
 
-                if index != totalCount {
-                        b.WriteString(" AND ")
-                }
+		if index != totalCount {
+			b.WriteString(" AND ")
+		}
 
-        }
+	}
 
-        return b.String()
+	return b.String()
 }
 
 // GetInFilterValue constructs the 'In' Clause value
 func GetInFilterValue(values []string) string {
-        index := 0
-        totalCount := len(values)
-        var b strings.Builder
-        for _, val := range values {
-                index++
-                b.WriteString("'" + strings.TrimSpace(val) + "'")
-                if index != totalCount {
-                        b.WriteString(",")
-                }
-        }
+	index := 0
+	totalCount := len(values)
+	var b strings.Builder
+	for _, val := range values {
+		index++
+		b.WriteString("'" + strings.TrimSpace(val) + "'")
+		if index != totalCount {
+			b.WriteString(",")
+		}
+	}
 
-        return b.String()
+	return b.String()
 }
 
 // ProcessCustomFields replaces custom field names with cf[customFieldId] for searching purpose
 // Gets the values from customFieldChannel and replaces the names and assign it to processor channel
 func ProcessCustomFields(config Configuration, customFieldChannel chan map[string]string, customFieldProcessorChannel chan ProcessedData) {
-        customFieldMap, ok := <-customFieldChannel
-        substitutedFilters := make(map[string]string)
-        substitutedfieldsToRetrieve := make([]string, 0)
+	customFieldMap, ok := <-customFieldChannel
+	substitutedFilters := make(map[string]string)
+	substitutedfieldsToRetrieve := make([]string, 0)
 
-        if ok {
-                for k, v := range config.Filters {
-                        _, ok := customFieldMap[k]
-                        if ok {
-                                equivalentFieldId := customFieldMap[k]
-                                keyName := "cf[" + strings.Replace(equivalentFieldId, "customfield_", "", -1) + "]"
-                                substitutedFilters[keyName] = v.(string)
-                        } else {
-                                substitutedFilters[k] = v.(string)
-                        }
-                }
+	if ok {
+		for k, v := range config.Filters {
+			_, ok := customFieldMap[k]
+			if ok {
+				equivalentFieldId := customFieldMap[k]
+				keyName := "cf[" + strings.Replace(equivalentFieldId, "customfield_", "", -1) + "]"
+				substitutedFilters[keyName] = v.(string)
+			} else {
+				substitutedFilters[k] = v.(string)
+			}
+		}
 
-                for _, v := range config.FieldsToRetrieve {
-                        _, ok = customFieldMap[v]
-                        if ok {
-                                equivalentFieldId := customFieldMap[v]
-                                substitutedfieldsToRetrieve = append(substitutedfieldsToRetrieve, fmt.Sprint(equivalentFieldId))
-                        } else {
-                                substitutedfieldsToRetrieve = append(substitutedfieldsToRetrieve, v)
-                        }
-                }
-        }
-        processedValues := ProcessedData{filters: substitutedFilters, fields: substitutedfieldsToRetrieve}
-        customFieldProcessorChannel <- processedValues
+		for _, v := range config.FieldsToRetrieve {
+			_, ok = customFieldMap[v]
+			if ok {
+				equivalentFieldId := customFieldMap[v]
+				substitutedfieldsToRetrieve = append(substitutedfieldsToRetrieve, fmt.Sprint(equivalentFieldId))
+			} else {
+				substitutedfieldsToRetrieve = append(substitutedfieldsToRetrieve, v)
+			}
+		}
+	}
+	processedValues := ProcessedData{filters: substitutedFilters, fields: substitutedfieldsToRetrieve}
+	customFieldProcessorChannel <- processedValues
 }
 
 // InitiateSearch initiates the search process
 func InitiateSearch(config Configuration, customFieldProcessorChannel chan ProcessedData, issueRetrievedChannel chan JiraIssue, totalRestCalls *int) {
-        processedValues, ok := <-customFieldProcessorChannel
-        if ok {
-                *totalRestCalls++
-                go SearchIssues(config, GetJql(processedValues.filters), processedValues.fields, issueRetrievedChannel)
-        }
+	processedValues, ok := <-customFieldProcessorChannel
+	if ok {
+		*totalRestCalls++
+		go SearchIssues(config, GetJql(processedValues.filters), processedValues.fields, issueRetrievedChannel)
+	}
 }
