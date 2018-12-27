1eb33b39ec1d8dde744840cdef9d1386a6bd5e01
raghu.chandramoule <raghu.chandramoule@ASPIRE1787>
2018-12-27 15:24:21 +0530

    Formatted the files using goFmt

--- jiraSearch.go
+++ jiraSearch.go
@@ -1,232 +1,213 @@
 package main
 
-import(
-	"fmt"
-	"strings"
-	"time"
+import (
+        "fmt"
+        "strings"
+        "time"
 
-	 ui "github.com/gizak/termui"
+        ui "github.com/gizak/termui"
 )
 
 // ProcessedData represents the filters and fields after replacing custom field ids
-type ProcessedData struct{
-	filters map[string]string
-	fields []string
+type ProcessedData struct {
+        filters map[string]string
+        fields  []string
 }
 
 // Configuration represents the configuration file 'config.json'
-type Configuration struct{
-	JiraUrl string
-	Credentials Credentials
-	Filters map[string]interface{}
-	FieldsToRetrieve []string
-	DownloadPath string
-	AuthToken string
+type Configuration struct {
+        JiraUrl          string
+        Credentials      Credentials
+        Filters          map[string]interface{}
+        FieldsToRetrieve []string
+        DownloadPath     string
+        AuthToken        string
 }
 
 // Credentials of the user
-type Credentials struct{
-	Username string
-	Password string
+type Credentials struct {
+        Username string
+        Password string
 }
 
 // SubTask of a Jira Issue
-type SubTask struct{
-	Type string
-	AssigneeName string
-	TotalHours string
-	Name string
+type SubTask struct {
+        Type         string
+        AssigneeName string
+        TotalHours   string
+        Name         string
 }
 
 // JiraIssue represents an issue in Jira
-type JiraIssue struct{
-	Data map[string]interface{}
-	SubTasks []SubTask
-	Fields []string
-	AssigneeName string
+type JiraIssue struct {
+        Data         map[string]interface{}
+        SubTasks     []SubTask
+        Fields       []string
+        AssigneeName string
 }
 
+func main() {
+        start := time.Now()
 
+        // Initialize necessary variables
+        customFieldChannel := make(chan map[string]string)
+        customFieldProcessorChannel := make(chan ProcessedData)
+        issueRetrievedChannel := make(chan JiraIssue)
+        finalIssueChannel := make(chan JiraIssue)
+        outputData := make([][]string, 0)
+        issueCount := 0
+        totalRestCalls := 0
+        totalIssueCount := 0
 
-func main(){
-	start := time.Now()
+        // Init the termui
+        // for displaying the output statistics in terminal
+        err := ui.Init()
+        HandleError(err)
+        defer ui.Close()
+        g, bc := GetProgressAndStatisticsBar()
 
+        // Read config from the 'config.json' file
+        config := ReadConfigFromFile("config.json")
 
-	// Initialize necessary variables
-	customFieldChannel := make(chan map[string]string)
-	customFieldProcessorChannel := make(chan ProcessedData)
-	issueRetrievedChannel := make(chan JiraIssue)
-	finalIssueChannel := make(chan JiraIssue)
-	outputData := make([][]string, 0)
-	issueCount := 0
-	totalRestCalls := 0
-	totalIssueCount := 0
+        // get all the custom fields first and populate in customFieldChannel
+        totalRestCalls++
+        go GetCustomFields(config, customFieldChannel)
 
-	// Init the termui
-	// for displaying the output statistics in terminal
-	err := ui.Init()
-	HandleError(err)
-	defer ui.Close()
-	g, bc := GetProgressAndStatisticsBar()
+        // listen to custom fields channel and process the data
+        go ProcessCustomFields(config, customFieldChannel, customFieldProcessorChannel)
 
-	// Read config from the 'config.json' file
-	config := ReadConfigFromFile("config.json")
-	
-	// get all the custom fields first and populate in customFieldChannel
-	totalRestCalls++
-	go GetCustomFields(config, customFieldChannel)
+        // listen to processed data and intiate the search based on it
+        go InitiateSearch(config, customFieldProcessorChannel, issueRetrievedChannel, &totalRestCalls)
 
-	// listen to custom fields channel and process the data
-	go ProcessCustomFields(config, customFieldChannel, customFieldProcessorChannel)
+        //prepare output with the headers first
+        headers := config.FieldsToRetrieve
+        outputData = append(outputData, headers)
 
-	// listen to processed data and intiate the search based on it
-	go InitiateSearch(config, customFieldProcessorChannel, issueRetrievedChannel, &totalRestCalls)
+        for {
+                select {
 
-	//prepare output with the headers first
-	headers := config.FieldsToRetrieve
-	outputData = append(outputData, headers)
+                case issue := <-issueRetrievedChannel:
+                        {
+                                totalIssueCount++
+                                issueCount++
+                                DisplayProgressAndStatistics(totalIssueCount, -1, totalRestCalls, int(time.Since(start).Seconds()), g, bc)
+                                includeChangeLog := strings.Contains(config.Filters["IssueType"].(string), "Bug")
 
-	for {
-	select {
-			
-		case issue := <- issueRetrievedChannel:{
-			totalIssueCount ++
-			issueCount++
-			DisplayProgressAndStatistics(totalIssueCount,-1,totalRestCalls,int(time.Since(start).Seconds()) , g, bc)
-			includeChangeLog := strings.Contains(config.Filters["IssueType"].(string),"Bug")
+                                // listen to issue retrieved channel and assign the sub tasks for it
+                                go GetSubTasksForIssue(config, issue, finalIssueChannel, includeChangeLog, &totalRestCalls)
+                        }
 
-			// listen to issue retrieved channel and assign the sub tasks for it
-		    go GetSubTasksForIssue(config, issue , finalIssueChannel, includeChangeLog, &totalRestCalls)
-		}
+                case finalIssue := <-finalIssueChannel:
+                        {
+                                issueCount--
+                                fieldValues := make([]string, 0)
 
-    	case finalIssue := <- finalIssueChannel:{
-			issueCount --
-			fieldValues := make([]string,0)
+                                // Listen to final populated issue and prepare the output for all the fields mentioned in the configuration
+                                for _, field := range finalIssue.Fields {
+                                        val, ok := finalIssue.Data[field]
+                                        if ok {
+                                                fieldValues = append(fieldValues, strings.Replace(val.(string), ",", "", -1))
+                                        } else {
+                                                fieldValues = append(fieldValues, GetFieldValue(field, finalIssue))
+                                        }
+                                }
+                                if len(fieldValues) > 0 {
+                                        outputData = append(outputData, fieldValues)
+                                }
 
-		   // Listen to final populated issue and prepare the output for all the fields mentioned in the configuration
-           for _, field := range finalIssue.Fields{
-			 val, ok := finalIssue.Data[field]
-			 if(ok){
-				fieldValues = append(fieldValues, strings.Replace(val.(string),",","",-1))
-			 }else{
-				fieldValues = append(fieldValues, GetFieldValue(field, finalIssue))
-			}
-		 }
-		 if(len(fieldValues) > 0 ){
-			outputData = append(outputData, fieldValues)
-		 }
-		 
-		 DisplayProgressAndStatistics(totalIssueCount,issueCount,totalRestCalls,int(time.Since(start).Seconds()) , g, bc)
+                                DisplayProgressAndStatistics(totalIssueCount, issueCount, totalRestCalls, int(time.Since(start).Seconds()), g, bc)
 
-		}
-		
-		
-		if issueCount == 0 {
-			// Once all the issues are processed, flush out the output data to the csv file mentioned in the configuration 'DownloadPath'
-			WriteToCsv(outputData, config.DownloadPath)
-			DisplayProgressAndStatistics(totalIssueCount,issueCount,totalRestCalls,int(time.Since(start).Seconds()) , g, bc)
-			fmt.Println("Download complete!!")
-			break
+                        }
+
+                        if issueCount == 0 {
+                                // Once all the issues are processed, flush out the output data to the csv file mentioned in the configuration 'DownloadPath'
+                                WriteToCsv(outputData, config.DownloadPath)
+                                DisplayProgressAndStatistics(totalIssueCount, issueCount, totalRestCalls, int(time.Since(start).Seconds()), g, bc)
+                                fmt.Println("Download complete!!")
+                                break
+                        }
+
+                }
         }
-	
-	}
-  }
 }
-
 
 // GetJql constructs the JQL string based on filters in the configuration
-func GetJql(filters map[string]string)string{
-	index := 0
-	totalCount := len(filters)
-	var b strings.Builder
-	for k, v := range filters{
-		index++
-		if(strings.Contains(v,",")){
-           valSlice := strings.Split(v,",")
-		   b.WriteString(k + " in ("+ GetInFilterValue(valSlice) +")")
-		}else{
-		   b.WriteString(k + "=" + "'" + v + "'")
-		}
+func GetJql(filters map[string]string) string {
+        index := 0
+        totalCount := len(filters)
+        var b strings.Builder
+        for k, v := range filters {
+                index++
+                if strings.Contains(v, ",") {
+                        valSlice := strings.Split(v, ",")
+                        b.WriteString(k + " in (" + GetInFilterValue(valSlice) + ")")
+                } else {
+                        b.WriteString(k + "=" + "'" + v + "'")
+                }
 
-	if(index != totalCount){
-		b.WriteString(" AND ")
-	}
+                if index != totalCount {
+                        b.WriteString(" AND ")
+                }
 
-	}
+        }
 
-	return b.String();
+        return b.String()
 }
-
 
 // GetInFilterValue constructs the 'In' Clause value
-func GetInFilterValue(values []string)string{
-	index := 0
-	totalCount := len(values)
-	var b strings.Builder
-	for _, val := range values{
-		index++
-		b.WriteString("'" + strings.TrimSpace(val) + "'")
-		if(index != totalCount){
-			b.WriteString(",")
-		}
-	}
+func GetInFilterValue(values []string) string {
+        index := 0
+        totalCount := len(values)
+        var b strings.Builder
+        for _, val := range values {
+                index++
+                b.WriteString("'" + strings.TrimSpace(val) + "'")
+                if index != totalCount {
+                        b.WriteString(",")
+                }
+        }
 
-	return b.String()
+        return b.String()
 }
-
 
 // ProcessCustomFields replaces custom field names with cf[customFieldId] for searching purpose
 // Gets the values from customFieldChannel and replaces the names and assign it to processor channel
-func ProcessCustomFields(config Configuration, customFieldChannel chan map[string]string, customFieldProcessorChannel chan ProcessedData){
-	customFieldMap, ok := <- customFieldChannel
-	substitutedFilters := make(map[string]string)
-	substitutedfieldsToRetrieve := make([]string,0)
+func ProcessCustomFields(config Configuration, customFieldChannel chan map[string]string, customFieldProcessorChannel chan ProcessedData) {
+        customFieldMap, ok := <-customFieldChannel
+        substitutedFilters := make(map[string]string)
+        substitutedfieldsToRetrieve := make([]string, 0)
 
-	
-	if(ok){
-		  for k, v := range config.Filters{
-			  _, ok := customFieldMap[k]
-			  if(ok){
-				  equivalentFieldId := customFieldMap[k]
-				  keyName := "cf[" + strings.Replace(equivalentFieldId,"customfield_","",-1) + "]"
-                  substitutedFilters[keyName] = v.(string)
-			  }else{
-			      substitutedFilters[k] = v.(string)
-			  }
-		  }
+        if ok {
+                for k, v := range config.Filters {
+                        _, ok := customFieldMap[k]
+                        if ok {
+                                equivalentFieldId := customFieldMap[k]
+                                keyName := "cf[" + strings.Replace(equivalentFieldId, "customfield_", "", -1) + "]"
+                                substitutedFilters[keyName] = v.(string)
+                        } else {
+                                substitutedFilters[k] = v.(string)
+                        }
+                }
 
-		  for _, v := range config.FieldsToRetrieve{
-			  _, ok = customFieldMap[v]
-			  if(ok){
-				  equivalentFieldId := customFieldMap[v]
-				  substitutedfieldsToRetrieve = append(substitutedfieldsToRetrieve, fmt.Sprint(equivalentFieldId))
-			  }else{
-                substitutedfieldsToRetrieve = append(substitutedfieldsToRetrieve, v)
-			  }
-		  }
-		}
-		processedValues := ProcessedData{ filters: substitutedFilters, fields: substitutedfieldsToRetrieve}
-		customFieldProcessorChannel <- processedValues
+                for _, v := range config.FieldsToRetrieve {
+                        _, ok = customFieldMap[v]
+                        if ok {
+                                equivalentFieldId := customFieldMap[v]
+                                substitutedfieldsToRetrieve = append(substitutedfieldsToRetrieve, fmt.Sprint(equivalentFieldId))
+                        } else {
+                                substitutedfieldsToRetrieve = append(substitutedfieldsToRetrieve, v)
+                        }
+                }
+        }
+        processedValues := ProcessedData{filters: substitutedFilters, fields: substitutedfieldsToRetrieve}
+        customFieldProcessorChannel <- processedValues
 }
-
 
 // InitiateSearch initiates the search process
-func InitiateSearch(config Configuration, customFieldProcessorChannel chan ProcessedData, issueRetrievedChannel chan JiraIssue, totalRestCalls *int){
-		processedValues, ok := <- customFieldProcessorChannel
-		if(ok){
-			*totalRestCalls++
-			go SearchIssues(config, GetJql(processedValues.filters), processedValues.fields, issueRetrievedChannel)
-		}
+func InitiateSearch(config Configuration, customFieldProcessorChannel chan ProcessedData, issueRetrievedChannel chan JiraIssue, totalRestCalls *int) {
+        processedValues, ok := <-customFieldProcessorChannel
+        if ok {
+                *totalRestCalls++
+                go SearchIssues(config, GetJql(processedValues.filters), processedValues.fields, issueRetrievedChannel)
+        }
 }
-
-
-
-
-
-
-
-
-
-
-
-
--- utility_test.go
+++ utility_test.go
@@ -2,150 +2,140 @@ package main
 
 import "testing"
 
-func TestGetNumberOfFunctionalBugs(t *testing.T){
-	subTasks := make([]SubTask, 0)
-	subTask1 := SubTask{ Type :"Functional Bug"}
-	subTask2 := SubTask{Type : "Story"}
-	
-	subTasks = append(subTasks, subTask1, subTask2)
+func TestGetNumberOfFunctionalBugs(t *testing.T) {
+        subTasks := make([]SubTask, 0)
+        subTask1 := SubTask{Type: "Functional Bug"}
+        subTask2 := SubTask{Type: "Story"}
 
-	numberOfFunctionalBugs := GetNumberOfFunctionalBugs(subTasks)
+        subTasks = append(subTasks, subTask1, subTask2)
 
-	if(numberOfFunctionalBugs != 1){
-		t.Errorf("The number of funcational bugs is incorrect, got : %d, want : %d",numberOfFunctionalBugs,1)
-	}
+        numberOfFunctionalBugs := GetNumberOfFunctionalBugs(subTasks)
+
+        if numberOfFunctionalBugs != 1 {
+                t.Errorf("The number of funcational bugs is incorrect, got : %d, want : %d", numberOfFunctionalBugs, 1)
+        }
 }
 
+func TestGetDevTaskAssigneeNameReturnsDevName(t *testing.T) {
+        subTasks := make([]SubTask, 0)
+        subTask1 := SubTask{Name: "Dev : Analysis", AssigneeName: "Dev1"}
 
-func TestGetDevTaskAssigneeNameReturnsDevName(t *testing.T){
-	subTasks := make([]SubTask, 0)
-	subTask1 := SubTask{ Name : "Dev : Analysis", AssigneeName:"Dev1"}
+        subTasks = append(subTasks, subTask1)
 
-	subTasks = append(subTasks, subTask1)
+        devTaskAssigneeName := GetDevTaskAssigneeName(subTasks)
 
-	devTaskAssigneeName := GetDevTaskAssigneeName(subTasks)
-
-	if(devTaskAssigneeName != "Dev1"){
-		t.Errorf("The dev task assignee name is wrong, got : %s, want : %s",devTaskAssigneeName,"Dev1")
-	}
+        if devTaskAssigneeName != "Dev1" {
+                t.Errorf("The dev task assignee name is wrong, got : %s, want : %s", devTaskAssigneeName, "Dev1")
+        }
 }
 
+func TestGetDevTaskAssigneeNameNotReturnsReviewerName(t *testing.T) {
+        subTasks := make([]SubTask, 0)
+        subTask1 := SubTask{Name: "Dev : Do code review", AssigneeName: "Dev1"}
+        subTask2 := SubTask{Name: "Dev : Coding", AssigneeName: "Dev2"}
 
-func TestGetDevTaskAssigneeNameNotReturnsReviewerName(t *testing.T){
-	subTasks := make([]SubTask, 0)
-	subTask1 := SubTask{ Name : "Dev : Do code review", AssigneeName:"Dev1"}
-	subTask2 := SubTask{ Name : "Dev : Coding", AssigneeName:"Dev2"}
+        subTasks = append(subTasks, subTask1, subTask2)
 
-	subTasks = append(subTasks, subTask1, subTask2)
+        devTaskAssigneeName := GetDevTaskAssigneeName(subTasks)
 
-	devTaskAssigneeName := GetDevTaskAssigneeName(subTasks)
-
-	if(devTaskAssigneeName != "Dev2"){
-		t.Errorf("The dev task assignee name is wrong, got : %s, want : %s",devTaskAssigneeName,"Dev2")
-	}
+        if devTaskAssigneeName != "Dev2" {
+                t.Errorf("The dev task assignee name is wrong, got : %s, want : %s", devTaskAssigneeName, "Dev2")
+        }
 }
 
+func TestGetDevTaskAssigneeNameEmpty(t *testing.T) {
+        subTasks := make([]SubTask, 0)
+        subTask1 := SubTask{Name: "QA : Testing", AssigneeName: "Dev1"}
+        subTask2 := SubTask{Name: "UX : Review", AssigneeName: "Dev2"}
 
+        subTasks = append(subTasks, subTask1, subTask2)
 
-func TestGetDevTaskAssigneeNameEmpty(t *testing.T){
-	subTasks := make([]SubTask, 0)
-	subTask1 := SubTask{ Name : "QA : Testing", AssigneeName:"Dev1"}
-	subTask2 := SubTask{ Name : "UX : Review", AssigneeName:"Dev2"}
+        devTaskAssigneeName := GetDevTaskAssigneeName(subTasks)
 
-	subTasks = append(subTasks, subTask1, subTask2)
-
-	devTaskAssigneeName := GetDevTaskAssigneeName(subTasks)
-
-	if(devTaskAssigneeName != "N/A"){
-		t.Errorf("The dev task assignee name is wrong, got : %s, want : %s",devTaskAssigneeName,"N/A")
-	}
+        if devTaskAssigneeName != "N/A" {
+                t.Errorf("The dev task assignee name is wrong, got : %s, want : %s", devTaskAssigneeName, "N/A")
+        }
 }
 
+func TestComplexityBasedOnDevEstimates(t *testing.T) {
+        subTasks := make([]SubTask, 0)
+        subTask1 := SubTask{Name: "Dev : Analysis", TotalHours: "8h"}
+        subTask2 := SubTask{Name: "Dev : Coding", TotalHours: "12h"}
+        subTask3 := SubTask{Name: "Dev : UnitTesting", TotalHours: "12h"}
 
-func TestComplexityBasedOnDevEstimates(t *testing.T){
-	subTasks := make([]SubTask, 0)
-	subTask1 := SubTask{ Name : "Dev : Analysis", TotalHours:"8h"}
-	subTask2 := SubTask{ Name : "Dev : Coding", TotalHours:"12h"}
-	subTask3 := SubTask{ Name : "Dev : UnitTesting", TotalHours:"12h"}
+        subTasks = append(subTasks, subTask1, subTask2, subTask3)
 
-	subTasks = append(subTasks, subTask1, subTask2, subTask3)
+        complexity := GetComplexityBasedOnDevEstimation(subTasks)
 
-	complexity := GetComplexityBasedOnDevEstimation(subTasks)
-
-	if(complexity != "Large"){
-		t.Errorf("Complexity calculation is wrong. got : %s, want : %s",complexity, "Large")
-	}
+        if complexity != "Large" {
+                t.Errorf("Complexity calculation is wrong. got : %s, want : %s", complexity, "Large")
+        }
 }
 
+func TestComplexityBasedOnDevEstimatesNotIncludesQATask(t *testing.T) {
+        subTasks := make([]SubTask, 0)
+        subTask1 := SubTask{Name: "Dev : Analysis", TotalHours: "8h"}
+        subTask2 := SubTask{Name: "Dev : Coding", TotalHours: "12h"}
+        subTask3 := SubTask{Name: "Dev : UnitTesting", TotalHours: "12h"}
+        subTask4 := SubTask{Name: "QA : Testing", TotalHours: "12h"}
 
-func TestComplexityBasedOnDevEstimatesNotIncludesQATask(t *testing.T){
-	subTasks := make([]SubTask, 0)
-	subTask1 := SubTask{ Name : "Dev : Analysis", TotalHours:"8h"}
-	subTask2 := SubTask{ Name : "Dev : Coding", TotalHours:"12h"}
-	subTask3 := SubTask{ Name : "Dev : UnitTesting", TotalHours:"12h"}
-	subTask4 := SubTask{ Name : "QA : Testing", TotalHours:"12h"}
+        subTasks = append(subTasks, subTask1, subTask2, subTask3, subTask4)
 
-	subTasks = append(subTasks, subTask1, subTask2, subTask3, subTask4)
+        complexity := GetComplexityBasedOnDevEstimation(subTasks)
 
-	complexity := GetComplexityBasedOnDevEstimation(subTasks)
-
-	if(complexity != "Large"){
-		t.Errorf("Complexity calculation is wrong. got : %s, want : %s",complexity, "Large")
-	}
+        if complexity != "Large" {
+                t.Errorf("Complexity calculation is wrong. got : %s, want : %s", complexity, "Large")
+        }
 }
 
+func TestComplexityBasedOnDevEstimatesNotIncludesReviewTask(t *testing.T) {
+        subTasks := make([]SubTask, 0)
+        subTask1 := SubTask{Name: "Dev : Analysis", TotalHours: "8h"}
+        subTask2 := SubTask{Name: "Dev : Coding", TotalHours: "12h"}
+        subTask3 := SubTask{Name: "Dev : UnitTesting", TotalHours: "12h"}
+        subTask4 := SubTask{Name: "Dev : code review", TotalHours: "12h"}
 
-func TestComplexityBasedOnDevEstimatesNotIncludesReviewTask(t *testing.T){
-	subTasks := make([]SubTask, 0)
-	subTask1 := SubTask{ Name : "Dev : Analysis", TotalHours:"8h"}
-	subTask2 := SubTask{ Name : "Dev : Coding", TotalHours:"12h"}
-	subTask3 := SubTask{ Name : "Dev : UnitTesting", TotalHours:"12h"}
-	subTask4 := SubTask{ Name : "Dev : code review", TotalHours:"12h"}
+        subTasks = append(subTasks, subTask1, subTask2, subTask3, subTask4)
 
-	subTasks = append(subTasks, subTask1, subTask2, subTask3, subTask4)
+        complexity := GetComplexityBasedOnDevEstimation(subTasks)
 
-	complexity := GetComplexityBasedOnDevEstimation(subTasks)
-
-	if(complexity != "Large"){
-		t.Errorf("Complexity calculation is wrong. got : %s, want : %s",complexity, "Large")
-	}
+        if complexity != "Large" {
+                t.Errorf("Complexity calculation is wrong. got : %s, want : %s", complexity, "Large")
+        }
 }
 
+func TestGetFieldValueAssigneeFromIssue(t *testing.T) {
+        issue := JiraIssue{AssigneeName: "Dev1"}
+        fieldValue := GetFieldValue("assignee", issue)
 
-func TestGetFieldValueAssigneeFromIssue(t *testing.T){
-	   issue :=  JiraIssue{ AssigneeName : "Dev1"}
-	   fieldValue := GetFieldValue("assignee", issue)
-
-	   if(fieldValue != "Dev1"){
-		   t.Errorf("Wrong assignee name, got : %s, want: %s", fieldValue, "Dev1")
-	   }
+        if fieldValue != "Dev1" {
+                t.Errorf("Wrong assignee name, got : %s, want: %s", fieldValue, "Dev1")
+        }
 }
 
+func TestGetFieldValueAssigneeFromSubTasks(t *testing.T) {
+        subTasks := make([]SubTask, 0)
+        subTask1 := SubTask{AssigneeName: "Dev1", Name: "Dev : coding"}
 
-func TestGetFieldValueAssigneeFromSubTasks(t *testing.T){
-	subTasks := make([]SubTask, 0)
-	subTask1 := SubTask{ AssigneeName : "Dev1", Name : "Dev : coding"}
+        subTasks = append(subTasks, subTask1)
+        issue := JiraIssue{SubTasks: subTasks}
+        fieldValue := GetFieldValue("assignee", issue)
 
-	subTasks = append(subTasks, subTask1)
-	issue :=  JiraIssue{ SubTasks : subTasks }
-	fieldValue := GetFieldValue("assignee", issue)
-
-	if(fieldValue != "Dev1"){
-		t.Errorf("Wrong assignee name, got : %s, want: %s", fieldValue, "Dev1")
-	}
+        if fieldValue != "Dev1" {
+                t.Errorf("Wrong assignee name, got : %s, want: %s", fieldValue, "Dev1")
+        }
 }
 
+func TestGetFieldValueFromField(t *testing.T) {
+        issueMap := make(map[string]interface{}, 0)
+        fieldsMap := make(map[string]interface{}, 0)
 
-func TestGetFieldValueFromField(t *testing.T){
-   issueMap := make(map[string]interface{}, 0)
-   fieldsMap := make(map[string]interface{}, 0)
+        fieldsMap["story points"] = 5
+        issueMap["fields"] = fieldsMap
 
-   fieldsMap["story points"] = 5
-   issueMap["fields"] = fieldsMap
+        fieldValue := GetValueFromField(issueMap, "story points")
 
-   fieldValue := GetValueFromField(issueMap, "story points")
-
-   if(fieldValue != "5"){
-	   t.Errorf("Wrong field value. got : %s, want : %s", fieldValue, "5")
-   }
+        if fieldValue != "5" {
+                t.Errorf("Wrong field value. got : %s, want : %s", fieldValue, "5")
+        }
 }
