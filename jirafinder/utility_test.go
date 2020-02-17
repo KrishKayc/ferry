ea879bf1574165aa1a22f8b04f634aef7c5acede
raghu.chandramoule <raghu.chandramoule@ASPIRE1787>
2020-02-17 09:22:16 +0530

    Updated unit tests

--- jirafinder/utility_test.go
+++ jirafinder/utility_test.go
@@ -1,229 +1,197 @@
 package jirafinder
 
-// import (
-// 	"testing"
-// )
+import (
+	"testing"
+)
 
-// type MockCommunicator struct {
-// 	Url       string
-// 	AuthToken string
-// }
+type MockHTTPRequest struct {
+}
 
-// type MockCommunicatorFieldRetrieval struct {
-// 	Url       string
-// 	AuthToken string
-// }
+func (mc *MockHTTPRequest) Send() []byte {
+	var jsonString = "{\"fields\" : { \"subTasks\" : [ {} ], \"issuetype\": { \"name\" : \"story\" } }}"
+	b := []byte(jsonString)
+	return b
+}
 
-// type MockCommunicatorFieldRetrievalStaticFields struct {
-// 	Url       string
-// 	AuthToken string
-// }
+func TestGetNumberOfFunctionalBugs(t *testing.T) {
+	subTasks := make([]SubTask, 0)
+	subTask1 := SubTask{Type: "Functional Bug"}
+	subTask2 := SubTask{Type: "Story"}
 
-// func (mc *MockCommunicator) CreateRequestAndGetResponse(apiPath string, params map[string]string) []byte {
-// 	var jsonString = "{\"fields\" : { \"subTasks\" : [ {} ], \"issuetype\": { \"name\" : \"story\" } }}"
-// 	b := []byte(jsonString)
-// 	return b
-// }
+	subTasks = append(subTasks, subTask1, subTask2)
 
-// func (mc *MockCommunicatorFieldRetrieval) CreateRequestAndGetResponse(apiPath string, params map[string]string) []byte {
-// 	var jsonString = "[{\"custom\" : true, \"name\" : \"testName\", \"id\" : \"testId\" }]"
-// 	b := []byte(jsonString)
-// 	return b
-// }
+	numberOfFunctionalBugs := getNumberOfFunctionalBugs(subTasks)
 
-// func (mc *MockCommunicatorFieldRetrievalStaticFields) CreateRequestAndGetResponse(apiPath string, params map[string]string) []byte {
-// 	var jsonString = "[{\"custom\" : true, \"name\" : \"testName\", \"id\" : \"testId\" }, {\"custom\" : false, \"name\" : \"testName\", \"id\" : \"testId\"}]"
-// 	b := []byte(jsonString)
-// 	return b
-// }
+	if numberOfFunctionalBugs != 1 {
+		t.Errorf("The number of funcational bugs is incorrect, got : %d, want : %d", numberOfFunctionalBugs, 1)
+	}
+}
 
-// func TestGetNumberOfFunctionalBugs(t *testing.T) {
-// 	subTasks := make([]SubTask, 0)
-// 	subTask1 := SubTask{Type: "Functional Bug"}
-// 	subTask2 := SubTask{Type: "Story"}
+func TestGetDevTaskAssigneeNameReturnsDevName(t *testing.T) {
+	subTasks := make([]SubTask, 0)
+	subTask1 := SubTask{Name: "Dev : Analysis", AssigneeName: "Dev1"}
 
-// 	subTasks = append(subTasks, subTask1, subTask2)
+	subTasks = append(subTasks, subTask1)
 
-// 	numberOfFunctionalBugs := GetNumberOfFunctionalBugs(subTasks)
+	devTaskAssigneeName := getDevTaskAssigneeName(subTasks)
 
-// 	if numberOfFunctionalBugs != 1 {
-// 		t.Errorf("The number of funcational bugs is incorrect, got : %d, want : %d", numberOfFunctionalBugs, 1)
-// 	}
-// }
+	if devTaskAssigneeName != "Dev1" {
+		t.Errorf("The dev task assignee name is wrong, got : %s, want : %s", devTaskAssigneeName, "Dev1")
+	}
+}
 
-// func TestGetDevTaskAssigneeNameReturnsDevName(t *testing.T) {
-// 	subTasks := make([]SubTask, 0)
-// 	subTask1 := SubTask{Name: "Dev : Analysis", AssigneeName: "Dev1"}
+func TestGetDevTaskAssigneeNameNotReturnsReviewerName(t *testing.T) {
+	subTasks := make([]SubTask, 0)
+	subTask1 := SubTask{Name: "Dev : Do code review", AssigneeName: "Dev1"}
+	subTask2 := SubTask{Name: "Dev : Coding", AssigneeName: "Dev2"}
 
-// 	subTasks = append(subTasks, subTask1)
+	subTasks = append(subTasks, subTask1, subTask2)
 
-// 	devTaskAssigneeName := GetDevTaskAssigneeName(subTasks)
+	devTaskAssigneeName := getDevTaskAssigneeName(subTasks)
 
-// 	if devTaskAssigneeName != "Dev1" {
-// 		t.Errorf("The dev task assignee name is wrong, got : %s, want : %s", devTaskAssigneeName, "Dev1")
-// 	}
-// }
+	if devTaskAssigneeName != "Dev2" {
+		t.Errorf("The dev task assignee name is wrong, got : %s, want : %s", devTaskAssigneeName, "Dev2")
+	}
+}
 
-// func TestGetDevTaskAssigneeNameNotReturnsReviewerName(t *testing.T) {
-// 	subTasks := make([]SubTask, 0)
-// 	subTask1 := SubTask{Name: "Dev : Do code review", AssigneeName: "Dev1"}
-// 	subTask2 := SubTask{Name: "Dev : Coding", AssigneeName: "Dev2"}
+func TestGetDevTaskAssigneeNameEmpty(t *testing.T) {
+	subTasks := make([]SubTask, 0)
+	subTask1 := SubTask{Name: "QA : Testing", AssigneeName: "Dev1"}
+	subTask2 := SubTask{Name: "UX : Review", AssigneeName: "Dev2"}
 
-// 	subTasks = append(subTasks, subTask1, subTask2)
+	subTasks = append(subTasks, subTask1, subTask2)
 
-// 	devTaskAssigneeName := GetDevTaskAssigneeName(subTasks)
+	devTaskAssigneeName := getDevTaskAssigneeName(subTasks)
 
-// 	if devTaskAssigneeName != "Dev2" {
-// 		t.Errorf("The dev task assignee name is wrong, got : %s, want : %s", devTaskAssigneeName, "Dev2")
-// 	}
-// }
+	if devTaskAssigneeName != "N/A" {
+		t.Errorf("The dev task assignee name is wrong, got : %s, want : %s", devTaskAssigneeName, "N/A")
+	}
+}
 
-// func TestGetDevTaskAssigneeNameEmpty(t *testing.T) {
-// 	subTasks := make([]SubTask, 0)
-// 	subTask1 := SubTask{Name: "QA : Testing", AssigneeName: "Dev1"}
-// 	subTask2 := SubTask{Name: "UX : Review", AssigneeName: "Dev2"}
+func TestComplexityBasedOnDevEstimates(t *testing.T) {
+	subTasks := make([]SubTask, 0)
+	subTask1 := SubTask{Name: "Dev : Analysis", TotalHours: "8h"}
+	subTask2 := SubTask{Name: "Dev : Coding", TotalHours: "12h"}
+	subTask3 := SubTask{Name: "Dev : UnitTesting", TotalHours: "12h"}
 
-// 	subTasks = append(subTasks, subTask1, subTask2)
+	subTasks = append(subTasks, subTask1, subTask2, subTask3)
 
-// 	devTaskAssigneeName := GetDevTaskAssigneeName(subTasks)
+	complexity := getComplexityBasedOnDevEstimation(subTasks)
 
-// 	if devTaskAssigneeName != "N/A" {
-// 		t.Errorf("The dev task assignee name is wrong, got : %s, want : %s", devTaskAssigneeName, "N/A")
-// 	}
-// }
+	if complexity != "Large" {
+		t.Errorf("Complexity calculation is wrong. got : %s, want : %s", complexity, "Large")
+	}
+}
 
-// func TestComplexityBasedOnDevEstimates(t *testing.T) {
-// 	subTasks := make([]SubTask, 0)
-// 	subTask1 := SubTask{Name: "Dev : Analysis", TotalHours: "8h"}
-// 	subTask2 := SubTask{Name: "Dev : Coding", TotalHours: "12h"}
-// 	subTask3 := SubTask{Name: "Dev : UnitTesting", TotalHours: "12h"}
+func TestComplexityBasedOnDevEstimatesNotIncludesQATask(t *testing.T) {
+	subTasks := make([]SubTask, 0)
+	subTask1 := SubTask{Name: "Dev : Analysis", TotalHours: "8h"}
+	subTask2 := SubTask{Name: "Dev : Coding", TotalHours: "12h"}
+	subTask3 := SubTask{Name: "Dev : UnitTesting", TotalHours: "12h"}
+	subTask4 := SubTask{Name: "QA : Testing", TotalHours: "12h"}
 
-// 	subTasks = append(subTasks, subTask1, subTask2, subTask3)
+	subTasks = append(subTasks, subTask1, subTask2, subTask3, subTask4)
 
-// 	complexity := GetComplexityBasedOnDevEstimation(subTasks)
+	complexity := getComplexityBasedOnDevEstimation(subTasks)
 
-// 	if complexity != "Large" {
-// 		t.Errorf("Complexity calculation is wrong. got : %s, want : %s", complexity, "Large")
-// 	}
-// }
+	if complexity != "Large" {
+		t.Errorf("Complexity calculation is wrong. got : %s, want : %s", complexity, "Large")
+	}
+}
 
-// func TestComplexityBasedOnDevEstimatesNotIncludesQATask(t *testing.T) {
-// 	subTasks := make([]SubTask, 0)
-// 	subTask1 := SubTask{Name: "Dev : Analysis", TotalHours: "8h"}
-// 	subTask2 := SubTask{Name: "Dev : Coding", TotalHours: "12h"}
-// 	subTask3 := SubTask{Name: "Dev : UnitTesting", TotalHours: "12h"}
-// 	subTask4 := SubTask{Name: "QA : Testing", TotalHours: "12h"}
+func TestComplexityBasedOnDevEstimatesNotIncludesReviewTask(t *testing.T) {
+	subTasks := make([]SubTask, 0)
+	subTask1 := SubTask{Name: "Dev : Analysis", TotalHours: "8h"}
+	subTask2 := SubTask{Name: "Dev : Coding", TotalHours: "12h"}
+	subTask3 := SubTask{Name: "Dev : UnitTesting", TotalHours: "12h"}
+	subTask4 := SubTask{Name: "Dev : code review", TotalHours: "12h"}
 
-// 	subTasks = append(subTasks, subTask1, subTask2, subTask3, subTask4)
+	subTasks = append(subTasks, subTask1, subTask2, subTask3, subTask4)
 
-// 	complexity := GetComplexityBasedOnDevEstimation(subTasks)
+	complexity := getComplexityBasedOnDevEstimation(subTasks)
 
-// 	if complexity != "Large" {
-// 		t.Errorf("Complexity calculation is wrong. got : %s, want : %s", complexity, "Large")
-// 	}
-// }
+	if complexity != "Large" {
+		t.Errorf("Complexity calculation is wrong. got : %s, want : %s", complexity, "Large")
+	}
+}
 
-// func TestComplexityBasedOnDevEstimatesNotIncludesReviewTask(t *testing.T) {
-// 	subTasks := make([]SubTask, 0)
-// 	subTask1 := SubTask{Name: "Dev : Analysis", TotalHours: "8h"}
-// 	subTask2 := SubTask{Name: "Dev : Coding", TotalHours: "12h"}
-// 	subTask3 := SubTask{Name: "Dev : UnitTesting", TotalHours: "12h"}
-// 	subTask4 := SubTask{Name: "Dev : code review", TotalHours: "12h"}
+func TestGetFieldValueAssigneeFromIssue(t *testing.T) {
+	issue := JiraIssue{AssigneeName: "Dev1"}
+	fieldValue := getFieldValue("assignee", issue)
 
-// 	subTasks = append(subTasks, subTask1, subTask2, subTask3, subTask4)
+	if fieldValue != "Dev1" {
+		t.Errorf("Wrong assignee name, got : %s, want: %s", fieldValue, "Dev1")
+	}
+}
 
-// 	complexity := GetComplexityBasedOnDevEstimation(subTasks)
+func TestGetFieldValueAssigneeFromSubTasks(t *testing.T) {
+	subTasks := make([]SubTask, 0)
+	subTask1 := SubTask{AssigneeName: "Dev1", Name: "Dev : coding"}
 
-// 	if complexity != "Large" {
-// 		t.Errorf("Complexity calculation is wrong. got : %s, want : %s", complexity, "Large")
-// 	}
-// }
+	subTasks = append(subTasks, subTask1)
+	issue := JiraIssue{SubTasks: subTasks}
+	fieldValue := getFieldValue("assignee", issue)
 
-// func TestGetFieldValueAssigneeFromIssue(t *testing.T) {
-// 	issue := JiraIssue{AssigneeName: "Dev1"}
-// 	fieldValue := GetFieldValue("assignee", issue)
+	if fieldValue != "Dev1" {
+		t.Errorf("Wrong assignee name, got : %s, want: %s", fieldValue, "Dev1")
+	}
+}
 
-// 	if fieldValue != "Dev1" {
-// 		t.Errorf("Wrong assignee name, got : %s, want: %s", fieldValue, "Dev1")
-// 	}
-// }
+func TestGetFieldValueFromField(t *testing.T) {
+	issueMap := make(map[string]interface{}, 0)
+	fieldsMap := make(map[string]interface{}, 0)
 
-// func TestGetFieldValueAssigneeFromSubTasks(t *testing.T) {
-// 	subTasks := make([]SubTask, 0)
-// 	subTask1 := SubTask{AssigneeName: "Dev1", Name: "Dev : coding"}
+	fieldsMap["story points"] = 5
+	issueMap["fields"] = fieldsMap
 
-// 	subTasks = append(subTasks, subTask1)
-// 	issue := JiraIssue{SubTasks: subTasks}
-// 	fieldValue := GetFieldValue("assignee", issue)
+	fieldValue := getValueFromField(issueMap, "story points")
 
-// 	if fieldValue != "Dev1" {
-// 		t.Errorf("Wrong assignee name, got : %s, want: %s", fieldValue, "Dev1")
-// 	}
-// }
+	if fieldValue != "5" {
+		t.Errorf("Wrong field value. got : %s, want : %s", fieldValue, "5")
+	}
+}
 
-// func TestGetFieldValueFromField(t *testing.T) {
-// 	issueMap := make(map[string]interface{}, 0)
-// 	fieldsMap := make(map[string]interface{}, 0)
+func TestGetNestedMapKeyName(t *testing.T) {
+	result := getNestedMapKeyName("Assignee")
 
-// 	fieldsMap["story points"] = 5
-// 	issueMap["fields"] = fieldsMap
+	if result != "displayName" {
+		ThrowError(t, "worng assignee nested name", "displayName", result)
+	}
 
-// 	fieldValue := GetValueFromField(issueMap, "story points")
+	result = getNestedMapKeyName("Reporter")
 
-// 	if fieldValue != "5" {
-// 		t.Errorf("Wrong field value. got : %s, want : %s", fieldValue, "5")
-// 	}
-// }
+	if result != "displayName" {
+		ThrowError(t, "worng reporter nested name", "displayName", result)
+	}
 
-// func TestReadConfig(t *testing.T) {
-// 	config := ReadConfig("config.json")
+	result = getNestedMapKeyName("IssueType")
 
-// 	if config.JiraUrl != "https://your-jira-url.com" {
-// 		t.Errorf("Wrong config jiraUrl, got : %s, want: %s", config.JiraUrl, "https://your-jira-url.com")
-// 	}
-// }
+	if result != "name" {
+		ThrowError(t, "worng IssueType nested name", "name", result)
+	}
 
-// func TestGetNestedMapKeyName(t *testing.T) {
-// 	result := GetNestedMapKeyName("Assignee")
+	result = getNestedMapKeyName("TimeTracking")
 
-// 	if result != "displayName" {
-// 		ThrowError(t, "worng assignee nested name", "displayName", result)
-// 	}
+	if result != "originalEstimate" {
+		ThrowError(t, "worng TimeTracking nested name", "originalEstimate", result)
+	}
 
-// 	result = GetNestedMapKeyName("Reporter")
+	result = getNestedMapKeyName("Status")
 
-// 	if result != "displayName" {
-// 		ThrowError(t, "worng reporter nested name", "displayName", result)
-// 	}
+	if result != "name" {
+		ThrowError(t, "worng Status nested name", "name", result)
+	}
 
-// 	result = GetNestedMapKeyName("IssueType")
+	result = getNestedMapKeyName("someCustomField")
 
-// 	if result != "name" {
-// 		ThrowError(t, "worng IssueType nested name", "name", result)
-// 	}
-
-// 	result = GetNestedMapKeyName("TimeTracking")
-
-// 	if result != "originalEstimate" {
-// 		ThrowError(t, "worng TimeTracking nested name", "originalEstimate", result)
-// 	}
-
-// 	result = GetNestedMapKeyName("Status")
-
-// 	if result != "name" {
-// 		ThrowError(t, "worng Status nested name", "name", result)
-// 	}
-
-// 	result = GetNestedMapKeyName("someCustomField")
-
-// 	if result != "value" {
-// 		ThrowError(t, "worng custom field value name", "value", result)
-// 	}
-// }
+	if result != "value" {
+		ThrowError(t, "worng custom field value name", "value", result)
+	}
+}
 
 // func TestGetIssue(t *testing.T) {
 // 	mc := MockCommunicator{}
-// 	issue := GetIssue(Configuration{}, "", false, &mc)
+// 	issue := getIssue(Configuration{}, "", false, &mc)
 
 // 	if issue["fields"] == nil {
 // 		t.Errorf("Get Issue not returning map")
@@ -232,7 +200,7 @@ package jirafinder
 
 // func TestGetIssueIncludingLog(t *testing.T) {
 // 	mc := MockCommunicator{}
-// 	issue := GetIssue(Configuration{}, "", true, &mc)
+// 	issue := getIssue(Configuration{}, "", true, &mc)
 
 // 	if issue["fields"] == nil {
 // 		t.Errorf("Get Issue not returning map")
@@ -243,7 +211,7 @@ package jirafinder
 // 	customFieldChannel := make(chan map[string]string)
 // 	mc := MockCommunicatorFieldRetrieval{}
 
-// 	go GetCustomFields(Configuration{}, customFieldChannel, &mc)
+// 	go getCustomFields(Configuration{}, customFieldChannel, &mc)
 
 // 	customFieldMap := <-customFieldChannel
 
@@ -264,6 +232,6 @@ package jirafinder
 // 		t.Errorf("Failed retieving custom field values into a map")
 // 	}
 // }
-// func ThrowError(t *testing.T, errorMsg string, expected string, actual string) {
-// 	t.Errorf("%s, got : %s, want: %s", errorMsg, actual, expected)
-// }
+func ThrowError(t *testing.T, errorMsg string, expected string, actual string) {
+	t.Errorf("%s, got : %s, want: %s", errorMsg, actual, expected)
+}
